637eab60248eb4a3bd8ba990a5207dbed0187126
jgonmor16 <jgonmor16@gmail.com>
2021-02-01 22:16:30 +0100

    Modify main to implement AUTO and MANUAL mode, controlled via web server
    New features:
    * The web server implements a modified RaspAP WiFi Configuration Panel
    which allows user to choose between modes (AUTO or MANUAL), turn on the
    device and set the starting and ending coordinates.
    * Main now waits until user sets the mode (AUTO or MANUAL) and turns
    the device on.
    * MANUAL mode allows user to control de robot's movement using the
    buttons added to the modified RaspAP panel.

--- src/main.py
+++ src/main.py
@@ -2,25 +2,47 @@ from atar import *
 from encoder import *
 from conv import *
 from blue import *
+from load import *
+import time as t
 
 def main():
 
-    # Conv Neural Network
-    maze = conv(3)
-    print(maze)
-   
-    # A* Pathfinding Algorithm
-    start = (7, 0)
-    end = (1, 0)
-    path = astar(maze, start, end)
-    print(path)
+    while(1):
+        while (load("on")=="1" and load("auto")=="1"):
+            # Load Data
+            coord = load("coord").split(' ')
+            x0, y0 = int(coord[0]), int(coord[1])
+            x1, y1 = int(coord[2]), int(coord[3])
 
-    # Encoder
-    orders = encoder(path)
-    print(orders)
+            # Conv Neural Network
+            maze = conv(3)
+            print(maze)
+           
+            # A* Pathfinding Algorithm
+            start = (x0, y0)
+            end = (x1, y1)
+            path = astar(maze, start, end)
+            print(path)
 
-    # Send orders via Bluetooth
-    blue(orders)
+            # Encoder
+            orders = encoder(path)
+            print(orders)
+
+            # Send orders via Bluetooth
+            blue(orders)
+
+            # Cambiamos a modo OFF
+            
+        while (load("on")=="1" and load("auto")=="0"):
+            # Division en coordenadas
+            move = load("move")
+            # Enviar por bluetoooth
+            blue(move)
+            # Comprobar OFF o AUTO
+            t.sleep(0.1)
+
+        t.sleep(0.3)
+
 
 if __name__ == '__main__':
     main()
